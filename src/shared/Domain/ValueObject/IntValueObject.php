decc1dbfd02876c8bb1f5eb89b9c3c72fc42302b
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:14:59 -0600

    Add shared value objects (int, string)

---
+++ src/shared/Domain/ValueObject/IntValueObject.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\shared\Domain\ValueObject;
+
+abstract class IntValueObject
+{
+
+    public function __construct(protected int $value)
+    {
+    }
+
+    public static function random(): self
+    {
+        return new static(random_int(1, 256));
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+}
