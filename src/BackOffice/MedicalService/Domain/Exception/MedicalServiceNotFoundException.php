2f2db119526b32e9c01a20ad4bdaa7c3ab85badc
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:11:20 -0600

    Add MedicalServiceNotFoundException

---
+++ src/BackOffice/MedicalService/Domain/Exception/MedicalServiceNotFoundException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\BackOffice\MedicalService\Domain\Exception;
+
+use Exception;
+
+class MedicalServiceNotFoundException extends Exception
+{
+
+    public function __construct()
+    {
+        parent::__construct(
+            'Medical Service Not Found',
+            404
+        );
+    }
+}
