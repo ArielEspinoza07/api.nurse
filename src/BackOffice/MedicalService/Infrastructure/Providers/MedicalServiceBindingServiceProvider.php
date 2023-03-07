a92aa493ccc40d5cf0311bcf42b88ac9a4a798bf
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:49:39 -0600

    Add MedicalServiceBindingServiceProvider

---
+++ src/BackOffice/MedicalService/Infrastructure/Providers/MedicalServiceBindingServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\BackOffice\MedicalService\Infrastructure\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Src\BackOffice\MedicalService\Domain\Repository\MedicalServiceRepository;
+use Src\BackOffice\MedicalService\Infrastructure\Persistence\Eloquent\EloquentMedicalServiceRepository;
+
+class MedicalServiceBindingServiceProvider extends ServiceProvider
+{
+
+    public array $bindings = [
+        MedicalServiceRepository::class => EloquentMedicalServiceRepository::class
+    ];
+
+    public function register(): void
+    {
+    }
+
+
+    public function boot(): void
+    {
+    }
+}
