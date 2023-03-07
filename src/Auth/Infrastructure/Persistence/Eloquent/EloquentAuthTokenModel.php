9b6c097270ebe581534ce2eb60934816a1a1ccc7
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:04:47 -0600

    Add EloquentAuthTokenModel

---
+++ src/Auth/Infrastructure/Persistence/Eloquent/EloquentAuthTokenModel.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Infrastructure\Persistence\Eloquent;
+
+use Src\shared\Infrastructure\Persistence\Eloquent\EloquentSanctumTokenBaseModel;
+
+/**
+ * Class EloquentAuthUserModel
+ *
+ * @property string $name
+ * @property string $password
+ *
+ * @package namespace Src\BackOffice\MedicalService\Infrastructure\Persistence\Eloquent
+ */
+class EloquentAuthTokenModel extends EloquentSanctumTokenBaseModel
+{
+    protected $primaryKey = 'id';
+
+    protected $table = 'personal_access_tokens';
+}
