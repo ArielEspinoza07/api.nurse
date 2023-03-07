4de69f0ca1e6ff4a7c83160d437c6465d615df43
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:05:56 -0600

    Add EloquentAuthUserRepository

---
+++ src/Auth/Infrastructure/Persistence/Eloquent/EloquentAuthUserRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Infrastructure\Persistence\Eloquent;
+
+use Src\Auth\Domain\AuthUserEmail;
+use Src\Auth\Domain\AuthUserId;
+use Src\Auth\Domain\AuthUserName;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Domain\Exception\InvalidAuthUserEmailException;
+use Src\Auth\Domain\Repository\AuthUserRepository;
+
+class EloquentAuthUserRepository implements AuthUserRepository
+{
+
+    public function findByEmail(AuthUserEmail $email): AuthUser
+    {
+        $model = EloquentAuthUserModel::query()
+            ->where('email', $email->value())
+            ->first();
+        if (!$model) {
+            throw new InvalidAuthUserEmailException();
+        }
+
+        return $this->createDomainEntityFromEloquentModel($model);
+    }
+
+    public function create(AuthUserName $name, AuthUserEmail $email, AuthUserPassword $password): AuthUser
+    {
+        $model = EloquentAuthUserModel::query()->create([
+            'name' => $name->value(),
+            'email' => $email->value(),
+            'password' => $password->value(),
+        ]);
+
+        return $this->createDomainEntityFromEloquentModel($model);
+    }
+
+    private function createDomainEntityFromEloquentModel(EloquentAuthUserModel $model): AuthUser
+    {
+        return AuthUser::create(
+            new AuthUserId($model->id),
+            new AuthUserName($model->name),
+            new AuthUserEmail($model->email),
+            new AuthUserPassword($model->password),
+            new AuthUserToken('')
+        );
+    }
+}
