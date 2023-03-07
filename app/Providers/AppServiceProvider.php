4cfcb51ac604e1263273d24a83810cba67d13475
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:46:15 -0600

    Modify AppServiceProvider
    add bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,22 @@
 
 namespace App\Providers;
 
+use App\Services\Rest\Json\Contract\ResponseBuilderContract;
+use App\Services\Rest\Json\Contract\ResponseContract;
+use App\Services\Rest\Json\Contract\ResponseExceptionHandlerContract;
+use App\Services\Rest\Json\Response;
+use App\Services\Rest\Json\ResponseBuilder;
+use App\Services\Rest\Json\ResponseExceptionHandler;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $bindings = [
+        ResponseBuilderContract::class => ResponseBuilder::class,
+        ResponseContract::class => Response::class,
+        ResponseExceptionHandlerContract::class => ResponseExceptionHandler::class,
+    ];
+
     /**
      * Register any application services.
      */
