8986fed98eb2d8de8f95dfc8417d9c1e1095c382
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:13:40 -0600

    Add AuthControllerTestBase

---
+++ tests/Functional/src/Auth/AuthControllerTestBase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Functional\src\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class AuthControllerTestBase extends TestCase
+{
+    use RefreshDatabase;
+
+    protected string $endpoint;
+
+    protected function registerUser(array $payload): void
+    {
+        $payload['name'] = $this->faker->name();
+        $payload['password_confirmation'] = $payload['password'];
+
+        $response = $this->post(route('api:v1:auth:register'), $payload);
+
+        if (Response::HTTP_OK !== $response->getStatusCode()) {
+            throw new RuntimeException('Error registering user');
+        }
+    }
+}
