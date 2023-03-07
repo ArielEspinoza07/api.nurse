400be8fb7d69fb5b44cebad3d233d428b3c83f91
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:07:21 -0600

    Add AuthUserBindingServiceProvider

---
+++ src/Auth/Infrastructure/Providers/AuthUserBindingServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Infrastructure\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Src\Auth\Domain\Contracts\PasswordHasherInterface;
+use Src\Auth\Domain\Contracts\TokenCreatorInterface;
+use Src\Auth\Domain\Repository\AuthUserRepository;
+use Src\Auth\Infrastructure\Hash\LaravelPasswordHasher;
+use Src\Auth\Infrastructure\Persistence\Eloquent\EloquentAuthUserRepository;
+use Src\Auth\Infrastructure\Token\LaravelSanctumTokenCreator;
+
+class AuthUserBindingServiceProvider extends ServiceProvider
+{
+    public array $bindings = [
+        AuthUserRepository::class => EloquentAuthUserRepository::class,
+        PasswordHasherInterface::class => LaravelPasswordHasher::class,
+        TokenCreatorInterface::class => LaravelSanctumTokenCreator::class,
+    ];
+
+    public function register(): void
+    {
+    }
+
+
+    public function boot(): void
+    {
+    }
+}
