874ff67b008f5b21dfc81eab9afa5e16e0c66e78
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:09:36 -0600

    Add AuthUserApplicationTestBase

---
+++ tests/Unit/src/Auth/AuthUserApplicationTestBase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\src\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserEmail;
+use Src\Auth\Domain\AuthUserId;
+use Src\Auth\Domain\AuthUserName;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Infrastructure\Hash\LaravelPasswordHasher;
+use Src\Auth\Infrastructure\Persistence\Eloquent\EloquentAuthUserModel;
+use Tests\TestCase;
+
+class AuthUserApplicationTestBase extends TestCase
+{
+    use RefreshDatabase;
+
+    protected string $tokenExample = '1|h0F5Nkx4vAjjPrmJw3RMDX9AH81kgl3xZVZoEzgi';
+    protected function createAutUser(array $payload): AuthUser
+    {
+        $model = EloquentAuthUserModel::query()->create(
+            [
+                'name' => $payload['name'],
+                'email' => $payload['email'],
+                'password' => (new LaravelPasswordHasher())
+                    ->hash(new AuthUserPassword($payload['password'])),
+            ]
+        );
+
+        return AuthUser::create(
+            new AuthUserId($model->id),
+            new AuthUserName($model->name),
+            new AuthUserEmail($model->email),
+            new AuthUserPassword($model->password),
+            new AuthUserToken(''),
+        );
+    }
+}
