28725495cd9b11eccd82136369eb2d47cce9cdfd
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:10:53 -0600

    Add MedicalServiceRepository

---
+++ src/BackOffice/MedicalService/Domain/Repository/MedicalServiceRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\BackOffice\MedicalService\Domain\Repository;
+
+use Src\BackOffice\MedicalService\Domain\MedicalService;
+use Src\BackOffice\MedicalService\Domain\MedicalServiceId;
+use Src\BackOffice\MedicalService\Domain\MedicalServiceIsActive;
+use Src\BackOffice\MedicalService\Domain\MedicalServiceName;
+use Src\shared\Domain\Criteria\Criteria;
+
+interface MedicalServiceRepository
+{
+
+    public function count(Criteria $criteria): int;
+
+    public function create(MedicalServiceName $name, MedicalServiceIsActive $active): MedicalService;
+
+    public function delete(MedicalServiceId $id): void;
+
+    public function findById(MedicalServiceId $id): MedicalService;
+
+    public function searchAll(): array;
+
+    public function searchByCriteria(Criteria $criteria): array;
+
+    public function update(MedicalService $medicalService): MedicalService;
+}
