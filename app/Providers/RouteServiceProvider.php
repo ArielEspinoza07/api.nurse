1e2b1f2ca2cfae77052bf6bc98a3579090c96bc8
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:47:30 -0600

    Modify RouteServiceProvider
    automatically add api v1 routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Finder\SplFileInfo as RouteFile;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -30,6 +32,13 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
+            foreach ($this->listRouteFiles('routes/v1') as $route) {
+                Route::middleware(['api',])//'auth:sanctum'
+                ->prefix('api/v1')
+                    ->as('api:v1:')
+                    ->namespace($this->namespace)
+                    ->group(base_path("routes/v1/$route"));
+            }
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
@@ -44,5 +53,12 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+    }
+
+    protected function listRouteFiles($directory = 'routes'): array
+    {
+        return array_map(function (RouteFile $file) {
+            return $file->getBasename();
+        }, File::allFiles(base_path($directory)));
     }
 }
