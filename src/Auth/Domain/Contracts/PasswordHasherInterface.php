eb1634c124ea93719d68721996e26522dcd9d8fc
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:02:10 -0600

    Add PasswordHasherInterface

---
+++ src/Auth/Domain/Contracts/PasswordHasherInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Domain\Contracts;
+
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserPassword;
+
+interface PasswordHasherInterface
+{
+
+    public function hash(AuthUserPassword $password): string;
+
+    public function check(AuthUser $user, AuthUserPassword $password): bool;
+}
