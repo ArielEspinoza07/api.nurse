6d8bdf58e8a4e576d5254ef34e62dcb2c52cca7b
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:55:22 -0600

    Add DeleteMedicalServiceController and his test

---
+++ src/BackOffice/MedicalService/Infrastructure/Http/Controllers/Rest/DeleteMedicalServiceController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\BackOffice\MedicalService\Infrastructure\Http\Controllers\Rest;
+
+use App\Services\Rest\Json\Contract\ResponseBuilderContract;
+use App\Services\Rest\Json\DTO\ResponseBuilderInputDTO;
+use Illuminate\Http\JsonResponse;
+use Src\BackOffice\MedicalService\Application\Delete\MedicalServiceDeletor;
+use Src\BackOffice\MedicalService\Domain\MedicalServiceId;
+use Src\shared\Infrastructure\Http\Controllers\BaseController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DeleteMedicalServiceController extends BaseController
+{
+
+    public function __construct(
+        private readonly MedicalServiceDeletor $service,
+        private readonly ResponseBuilderContract $response
+    ) {
+    }
+
+    public function __invoke(int $id): JsonResponse
+    {
+        $this->service->handle(new MedicalServiceId($id));
+
+        return $this->response
+            ->build(
+                new ResponseBuilderInputDTO(
+                    Response::HTTP_NO_CONTENT,
+                    'Deleted.'
+                )
+            );
+    }
+}
---
+++ tests/Functional/src/BackOffice/MedicalService/Controller/DeleteMedicalServiceMedicalServiceControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Functional\src\BackOffice\MedicalService\Controller;
+
+use Tests\Functional\src\BackOffice\MedicalServiceControllerTestBase;
+
+class DeleteMedicalServiceMedicalServiceControllerTest extends MedicalServiceControllerTestBase
+{
+    protected string $endpoint = 'api:v1:medical:service:delete';
+
+    public function test_delete_medical_service(): void
+    {
+        $medicalServiceId = $this->createMedicalService();
+
+        $response = $this->delete(route($this->endpoint, ['id' => $medicalServiceId]));
+
+        $response->assertNoContent();
+    }
+
+
+    public function test_delete_medical_service_with_non_existing_medical_service(): void
+    {
+        $medicalServiceId = rand(1, 10);
+
+        $response = $this->delete(route($this->endpoint, ['id' => $medicalServiceId]));
+
+        $response->assertNotFound();
+        $response->assertJsonStructure([
+            "success",
+            "message",
+            "data",
+        ]);
+
+        $this->assertEquals(false, $response->decodeResponseJson()['success']);
+        $this->assertEquals('Medical Service Not Found', $response->decodeResponseJson()['message']);
+    }
+}
