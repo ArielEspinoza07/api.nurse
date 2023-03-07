71ae28db61d8d1d1a6838c81bc39969d72572ecc
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:56:38 -0600

    Add SearchMedicalServiceController and his test

---
+++ src/BackOffice/MedicalService/Infrastructure/Http/Controllers/Rest/SearchMedicalServiceController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\BackOffice\MedicalService\Infrastructure\Http\Controllers\Rest;
+
+use App\Services\Rest\Json\Contract\ResponseBuilderContract;
+use App\Services\Rest\Json\DTO\ResponseBuilderInputDTO;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Src\BackOffice\MedicalService\Application\SearchAll\MedicalServiceSearcher;
+use Src\shared\Infrastructure\Criteria\CriteriaConverter;
+use Src\shared\Infrastructure\Criteria\DTO\SearchByCriteriaInputDTO;
+use Src\shared\Infrastructure\Http\Controllers\BaseController;
+use Symfony\Component\HttpFoundation\Response;
+
+class SearchMedicalServiceController extends BaseController
+{
+    public function __construct(
+        private readonly MedicalServiceSearcher $service,
+        private readonly ResponseBuilderContract $response
+    ) {
+    }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $medicalServices = $this->service->handle(
+            (new CriteriaConverter(
+                SearchByCriteriaInputDTO::createFromRequest($request)
+            ))->converter()
+        );
+        if (!empty($medicalServices['data'])) {
+            $medicalServices = [
+                'items' => $medicalServices['data'],
+                'meta' => [
+                    'page' => $medicalServices['current_page'],
+                    'per_page' => $medicalServices['per_page'],
+                    'page_count' => count($medicalServices['data']),
+                    'total_count' => $medicalServices['total'],
+                    'links' => $medicalServices['links'],
+                ],
+            ];
+        }
+
+        return $this->response
+            ->build(
+                new ResponseBuilderInputDTO(
+                    Response::HTTP_OK,
+                    'Retrieved all.',
+                    $medicalServices
+                )
+            );
+    }
+}
---
+++ tests/Functional/src/BackOffice/MedicalService/Controller/SearchMedicalServiceMedicalServiceControllerTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Tests\Functional\src\BackOffice\MedicalService\Controller;
+
+use Tests\Functional\src\BackOffice\MedicalServiceControllerTestBase;
+
+class SearchMedicalServiceMedicalServiceControllerTest extends MedicalServiceControllerTestBase
+{
+
+    protected string $endpoint = 'api:v1:medical:service:index';
+
+    public function test_search_all_medical_services_without_criteria(): void
+    {
+        $services = [
+            [
+                'name' => 'Emergency',
+            ],
+            [
+                'name' => 'Geriatric',
+            ],
+            [
+                'name' => 'Intensive Care Unit',
+            ],
+            [
+                'name' => 'Orthopaedic ',
+            ],
+            [
+                'name' => 'Pediatrics',
+            ],
+        ];
+
+        collect($services)
+            ->each(function ($service) {
+                $this->createMedicalService($service);
+            });
+
+        $response = $this->get(route($this->endpoint));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            "success",
+            "message",
+            "data",
+        ]);
+
+        $this->assertEquals(true, $response->decodeResponseJson()['success']);
+        $this->assertEquals('Retrieved all.', $response->decodeResponseJson()['message']);
+
+        $this->assertNotNull($response->decodeResponseJson()['data']);
+        $this->assertIsArray($response->decodeResponseJson()['data']);
+        $this->assertCount(count($services), $response->decodeResponseJson()['data']);
+    }
+
+    public function test_search_all_medical_services_with_criteria(): void
+    {
+        $services = [
+            [
+                'name' => 'Emergency',
+            ],
+            [
+                'name' => 'Geriatric',
+            ],
+            [
+                'name' => 'Intensive Care Unit',
+            ],
+            [
+                'name' => 'Orthopaedic ',
+            ],
+            [
+                'name' => 'Pediatrics',
+            ],
+        ];
+
+        collect($services)
+            ->each(function ($service) {
+                $this->createMedicalService($service);
+            });
+
+        $payload = [
+            'filters' => 'name:like:ic',
+            'order' => [
+                'by' => 'created_at',
+                'type' => 'asc',
+            ],
+            'page' => 1,
+            'limit' => 2,
+        ];
+
+        $queryString = http_build_query($payload);
+
+        $response = $this->get(route($this->endpoint).sprintf('?%s',$queryString));
+
+        $response->assertOk();
+        $response->assertJsonStructure([
+            "success",
+            "message",
+            "data" => [
+                "items",
+                "meta",
+            ],
+        ]);
+
+        $this->assertEquals(true, $response->decodeResponseJson()['success']);
+        $this->assertEquals('Retrieved all.', $response->decodeResponseJson()['message']);
+
+        $this->assertNotNull($response->decodeResponseJson()['data']);
+        $this->assertIsArray($response->decodeResponseJson()['data']);
+    }
+}
