c90d61e6d973473cc1193561bb5c773e8cf42400
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:16:44 -0600

    Add auth routes

---
+++ routes/v1/auth.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Src\Auth\Infrastructure\Http\Controllers\Rest\AuthenticateUserController;
+use Src\Auth\Infrastructure\Http\Controllers\Rest\RegisterUserController;
+
+Route::prefix('auth')
+    ->as('auth:')
+    ->group(function () {
+        Route::withoutMiddleware(['auth:sanctum'])
+            ->middleware(['guest'])
+            ->group(function () {
+                Route::post('/login', AuthenticateUserController::class)->name('login');
+                Route::post('/register', RegisterUserController::class)->name('register');
+            });
+    });
