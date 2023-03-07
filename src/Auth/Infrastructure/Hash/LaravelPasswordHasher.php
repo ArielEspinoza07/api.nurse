620527179ed9ec801536c58baf0da8c42db97d3a
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:06:57 -0600

    Add LaravelPasswordHasher

---
+++ src/Auth/Infrastructure/Hash/LaravelPasswordHasher.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Infrastructure\Hash;
+
+use Illuminate\Support\Facades\Hash;
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\Contracts\PasswordHasherInterface;
+
+class LaravelPasswordHasher implements PasswordHasherInterface
+{
+
+    public function hash(AuthUserPassword $password): string
+    {
+        return Hash::make($password->value());
+    }
+
+    public function check(AuthUser $user, AuthUserPassword $password): bool
+    {
+        return Hash::check($password->value(), $user->password()->value());
+    }
+}
