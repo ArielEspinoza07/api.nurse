00fc3048c0b6656bad9cbb980e8d7c240ceb058a
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 08:31:31 -0600

    Add shared EloquentBaseModel

---
+++ src/shared/Infrastructure/Persistence/Eloquent/EloquentBaseModel.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\shared\Infrastructure\Persistence\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EloquentBaseModel extends Model
+{
+
+}
