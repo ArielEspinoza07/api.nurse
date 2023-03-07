12921cce5ca2b146b034267bf6f1a405d444ec96
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:08:48 -0600

    Add UserAuthenticator and his test

---
+++ src/Auth/Application/Authenticate/UserAuthenticator.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Application\Authenticate;
+
+use Src\Auth\Domain\AuthUserEmail;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Domain\Contracts\PasswordHasherInterface;
+use Src\Auth\Domain\Contracts\TokenCreatorInterface;
+use Src\Auth\Domain\Exception\InvalidAuthUserPasswordException;
+use Src\Auth\Domain\Repository\AuthUserRepository;
+
+class UserAuthenticator
+{
+    public function __construct(
+        private readonly AuthUserRepository $repository,
+        private readonly PasswordHasherInterface $passwordHasher,
+        private readonly TokenCreatorInterface $tokenCreator
+    ) {
+    }
+
+    public function handle(AuthUserEmail $email, AuthUserPassword $password): AuthUserToken
+    {
+        $authUser = $this->repository->findByEmail($email);
+
+        if (!$this->passwordHasher->check($authUser, $password)) {
+            throw new InvalidAuthUserPasswordException($email);
+        }
+
+        return $this->tokenCreator->create($authUser);
+    }
+}
---
+++ tests/Unit/src/Auth/Application/UserAuthenticatorTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Tests\Unit\src\Auth\Application;
+
+use Illuminate\Support\Str;
+use Mockery;
+use Src\Auth\Application\Authenticate\UserAuthenticator;
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserEmail;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Domain\Contracts\PasswordHasherInterface;
+use Src\Auth\Domain\Contracts\TokenCreatorInterface;
+use Src\Auth\Domain\Repository\AuthUserRepository;
+use Tests\Unit\src\Auth\AuthUserApplicationTestBase;
+
+
+class UserAuthenticatorTest extends AuthUserApplicationTestBase
+{
+    public function test_user_authenticator_return_auth_user_token(): void
+    {
+        $payload = [
+            'name' => 'John Doe',
+            'email' => 'j.doe@gmail.com',
+            'password' => 'J.Doe1889',
+        ];
+        $authUser = $this->createAutUser($payload);
+
+        $repository = Mockery::mock(AuthUserRepository::class);
+        $this->app->instance(UserAuthenticator::class, $repository);
+
+        $repository->shouldReceive('findByEmail')
+            ->once()
+            ->with(
+                Mockery::on(function (AuthUserEmail $email) use ($authUser) {
+                    return $authUser->email()->value() === $email->value();
+                })
+            )
+            ->andReturn($authUser);
+
+        $passwordHasher = Mockery::mock(PasswordHasherInterface::class);
+        $this->app->instance(UserAuthenticator::class, $passwordHasher);
+
+        $passwordHasher->shouldReceive('check')
+            ->once()
+            ->with(
+                Mockery::on(function (AuthUser $user) use ($authUser) {
+                    return $authUser->id()->value() === $user->id()->value()
+                        && $authUser->email()->value() === $user->email()->value();
+                }),
+                Mockery::on(function (AuthUserPassword $password) use ($payload) {
+                    return $payload['password'] === $password->value();
+                }),
+            )
+            ->andReturn(true);
+
+        $tokenCreator = Mockery::mock(TokenCreatorInterface::class);
+        $this->app->instance(UserAuthenticator::class, $tokenCreator);
+
+        $tokenCreator->shouldReceive('create')
+            ->once()
+            ->with(
+                Mockery::on(function (AuthUser $user) use ($authUser) {
+                    return $authUser->id()->value() === $user->id()->value()
+                        && $authUser->email()->value() === $user->email()->value();
+                })
+            )
+            ->andReturn(new AuthUserToken($this->tokenExample));
+
+        $token = (new UserAuthenticator($repository, $passwordHasher, $tokenCreator))
+            ->handle(
+                new AuthUserEmail($payload['email']),
+                new AuthUserPassword($payload['password']),
+            );
+
+        $this->assertInstanceOf(AuthUserToken::class, $token);
+        $this->assertEquals(strlen($this->tokenExample), strlen($token->value()));
+    }
+}
