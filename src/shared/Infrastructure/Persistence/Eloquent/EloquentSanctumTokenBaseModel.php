84ec0e9bea6f3cedd9c6273a6e06290ba09ad966
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:04:28 -0600

    Add EloquentSanctumTokenBaseModel

---
+++ src/shared/Infrastructure/Persistence/Eloquent/EloquentSanctumTokenBaseModel.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\shared\Infrastructure\Persistence\Eloquent;
+
+use Laravel\Sanctum\PersonalAccessToken;
+
+class EloquentSanctumTokenBaseModel extends PersonalAccessToken
+{
+}
