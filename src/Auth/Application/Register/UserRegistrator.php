12e8a89e248e9c8631aaa9940112dc26414e5454
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-07 09:09:51 -0600

    Add UserRegistrator and his test

---
+++ src/Auth/Application/Register/UserRegistrator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Application\Register;
+
+use Src\Auth\Domain\AuthUserEmail;
+use Src\Auth\Domain\AuthUserName;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Domain\Contracts\PasswordHasherInterface;
+use Src\Auth\Domain\Contracts\TokenCreatorInterface;
+use Src\Auth\Domain\Repository\AuthUserRepository;
+
+class UserRegistrator
+{
+
+    public function __construct(
+        private readonly AuthUserRepository $repository,
+        private readonly PasswordHasherInterface $passwordHasher,
+        private readonly TokenCreatorInterface $tokenCreator,
+    ) {
+    }
+
+    public function handle(AuthUserName $name, AuthUserEmail $email, AuthUserPassword $password): AuthUserToken
+    {
+        return $this->tokenCreator
+            ->create(
+                $this->repository
+                    ->create(
+                        $name,
+                        $email,
+                        new AuthUserPassword($this->passwordHasher->hash($password))
+                    )
+            );
+    }
+}
---
+++ tests/Unit/src/Auth/Application/UserRegistratorTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Unit\src\Auth\Application;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
+use Mockery;
+use Src\Auth\Application\Register\UserRegistrator;
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserEmail;
+use Src\Auth\Domain\AuthUserName;
+use Src\Auth\Domain\AuthUserPassword;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Domain\Contracts\PasswordHasherInterface;
+use Src\Auth\Domain\Contracts\TokenCreatorInterface;
+use Src\Auth\Domain\Repository\AuthUserRepository;
+use Tests\Unit\src\Auth\AuthUserApplicationTestBase;
+
+class UserRegistratorTest extends AuthUserApplicationTestBase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function test_register_user(): void
+    {
+        $name = new AuthUserName($this->faker->name());
+        $email = new AuthUserEmail($this->faker->email());
+        $notEncryptedPassword = new AuthUserPassword('Password!1');
+
+        $notEncryptedAuthUser = AuthUser::createFromNameEmailAndPassword($name, $email, $notEncryptedPassword);
+
+        $passwordHasher = Mockery::mock(PasswordHasherInterface::class);
+        $this->app->instance(UserRegistrator::class, $passwordHasher);
+
+        $passwordHasher->shouldReceive('hash')
+            ->once()
+            ->with(
+                Mockery::on(function (AuthUserPassword $password) use ($notEncryptedPassword) {
+                    return $notEncryptedPassword->value() === $password->value();
+                })
+            )
+            ->andReturn($this->tokenExample);
+
+        $repository = Mockery::mock(AuthUserRepository::class);
+        $this->app->instance(UserRegistrator::class, $repository);
+        $repository->shouldReceive('create')
+            ->once()
+            ->with(
+                Mockery::on(function (AuthUserName $name) use ($notEncryptedAuthUser) {
+                    return $name->value() === $notEncryptedAuthUser->name()->value();
+                }),
+                Mockery::on(function (AuthUserEmail $email) use ($notEncryptedAuthUser) {
+                    return $email->value() === $notEncryptedAuthUser->email()->value();
+                }),
+                Mockery::on(function (AuthUserPassword $password) {
+                    return $password->value() !== null;
+                })
+            )
+            ->andReturn($notEncryptedAuthUser);
+
+        $tokenCreator = Mockery::mock(TokenCreatorInterface::class);
+        $this->app->instance(UserRegistrator::class, $tokenCreator);
+
+        $tokenCreator->shouldReceive('create')
+            ->once()
+            ->with(
+                Mockery::on(function (AuthUser $user) use ($notEncryptedAuthUser) {
+                    return $notEncryptedAuthUser->id()->value() === $user->id()->value()
+                        && $notEncryptedAuthUser->email()->value() === $user->email()->value()
+                        && $notEncryptedAuthUser->name()->value() === $user->name()->value();
+                })
+            )
+            ->andReturn(new AuthUserToken($this->tokenExample));
+
+        $token = (new UserRegistrator($repository, $passwordHasher, $tokenCreator))
+            ->handle(
+                $name,
+                $email,
+                $notEncryptedPassword
+            );
+
+        $this->assertInstanceOf(AuthUserToken::class, $token);
+        $this->assertEquals(strlen($this->tokenExample), strlen($token->value()));
+    }
+}
