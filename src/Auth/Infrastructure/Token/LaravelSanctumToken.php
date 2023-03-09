b212de9970b2c54553b5921d6ffb9aae67d2c546
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-09 10:37:15 -0600

    Modify LaravelSanctumTokenCreator
    rename to LaravelSanctumToken
    implements TokenDeletorInterface

---
+++ src/Auth/Infrastructure/Token/LaravelSanctumToken.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Infrastructure\Token;
+
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserToken;
+use Src\Auth\Domain\Contracts\TokenCreatorInterface;
+use Src\Auth\Domain\Contracts\TokenDeletorInterface;
+use Src\Auth\Infrastructure\Persistence\Eloquent\EloquentAuthUserModel;
+
+class LaravelSanctumToken implements TokenCreatorInterface, TokenDeletorInterface
+{
+
+    public function create(AuthUser $user): AuthUserToken
+    {
+        $model = EloquentAuthUserModel::query()
+            ->find(
+                $user->id()
+                    ->value()
+            );
+
+        if (!$model->wasRecentlyCreated) {
+            $model->currentAccessToken()
+                ->delete();
+        }
+
+        return new AuthUserToken($model->createPlainTextToken());
+    }
+
+
+    public function delete(AuthUser $user): void
+    {
+        EloquentAuthUserModel::query()
+            ->find(
+                $user->id()
+                    ->value()
+            )
+            ->tokens()
+            ->delete();
+    }
+}
