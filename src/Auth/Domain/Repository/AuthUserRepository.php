1f80204db1276a5e951137642d11049845309859
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-09 10:34:58 -0600

    Modify AuthUserRepository
    add method findById

--- src/Auth/Domain/Repository/AuthUserRepository.php
+++ src/Auth/Domain/Repository/AuthUserRepository.php
@@ -6,16 +6,22 @@ namespace Src\Auth\Domain\Repository;
 
 use Src\Auth\Domain\AuthUserEmail;
 use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserId;
 use Src\Auth\Domain\AuthUserName;
 use Src\Auth\Domain\AuthUserPassword;
 
 interface AuthUserRepository
 {
-    public function findByEmail(AuthUserEmail $email): AuthUser;
 
     public function create(
         AuthUserName $name,
         AuthUserEmail $email,
         AuthUserPassword $password
     );
+
+
+    public function findByEmail(AuthUserEmail $email): AuthUser;
+
+
+    public function findById(AuthUserId $id): AuthUser;
 }
