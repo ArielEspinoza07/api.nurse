5842861af11488719cd6607403f3e4b77c2f2e6c
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-09 10:36:05 -0600

    Add TokenDeletorInterface

---
+++ src/Auth/Domain/Contracts/TokenDeletorInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Auth\Domain\Contracts;
+
+use Src\Auth\Domain\AuthUser;
+use Src\Auth\Domain\AuthUserToken;
+
+interface TokenDeletorInterface
+{
+
+    public function delete(AuthUser $user): void;
+}
