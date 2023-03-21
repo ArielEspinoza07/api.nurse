a28e7993a27df937a38fd15347d2bed060ba9dbd
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-03-20 21:13:00 -0600

    change constructor visibility add create method

--- src/shared/Domain/Response/ResponseCode.php
+++ src/shared/Domain/Response/ResponseCode.php
@@ -11,9 +11,14 @@ class ResponseCode extends IntValueObject
 {
     use AssertNotNullable;
 
-    public function __construct(protected int $value)
+    private function __construct(protected int $value)
     {
         parent::__construct($this->value);
         $this->assertNotNull($this->value);
+    }
+
+    public static function create(int $value): self
+    {
+        return new static($value);
     }
 }
