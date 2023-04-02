0e029cd7f3e1ac4a4a91b3691a3c211fc0f61ba4
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-04-02 02:32:19 -0600

    modify html template

--- resources/views/emails/auth/verification-email/text-html.blade.php
+++ resources/views/emails/auth/verification-email/text-html.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{app()->getLocale()}}">
 <head>
     <title>Email Verification</title>
     <style>
@@ -7,9 +7,11 @@
             font-family: Arial, sans-serif;
             font-size: 16px;
             line-height: 1.5;
-            background-color: #f2f2f2;
-            padding: 20px;
+            margin-right: 20px;
+            margin-left: 20px;
+            margin-top: 60px;
         }
+
         h1 {
             font-size: 28px;
             font-weight: bold;
@@ -17,9 +19,11 @@
             margin-top: 0;
             margin-bottom: 20px;
         }
+
         p {
             margin: 0 0 20px;
         }
+
         a.button {
             display: inline-block;
             background-color: #008CBA;
@@ -33,17 +37,27 @@
             cursor: pointer;
             margin-bottom: 20px;
         }
+
         a.button:hover {
             background-color: #006688;
+        }
+
+        .container {
+            background-color: #f2f2f2;
+            text-align: center;
+            border-radius: 10px;
+            padding: 30px;
         }
     </style>
 </head>
 <body>
-<h1>Verify Your Email Address</h1>
-<p>Thank you for signing up with our service. To start using your account, we need to verify your email address.</p>
-<a href="{{$url}}" class="button">Verify Email Address</a>
-<p>If you did not create an account with us, please ignore this email.</p>
-<p>Thank you,</p>
-<p>The {{config('app.name')}} Team</p>
+<div class="container">
+    <h1>Verify Your Email Address</h1>
+    <p>Thank you for signing up with our service. To start using your account, we need to verify your email address.</p>
+    <a href="{{$url}}" class="button">Verify Email Address</a>
+    <p>If you did not create an account with us, please ignore this email.</p>
+    <p>Thank you,</p>
+    <p><b>The {{config('app.name')}} Team</b></p>
+</div>
 </body>
 </html>
