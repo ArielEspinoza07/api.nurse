f62c184c015e01363420646b0f2724c6baa5f2df
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-04-04 23:52:05 -0600

    fix exception

--- src/Auth/Domain/Exception/InvalidAuthUserPasswordException.php
+++ src/Auth/Domain/Exception/InvalidAuthUserPasswordException.php
@@ -6,10 +6,11 @@ namespace Src\Auth\Domain\Exception;
 
 use RuntimeException;
 use Src\Auth\Domain\AuthUserEmail;
+use Src\shared\Domain\EmailAddress;
 
 class InvalidAuthUserPasswordException extends RuntimeException
 {
-    public function __construct(AuthUserEmail $email)
+    public function __construct(EmailAddress $email)
     {
         parent::__construct(
             sprintf('Authentication failed: The credentials for [%s] are invalid', $email->value()),
