0335908dd79268d65b13a367593e963bf6c8a6fc
ArielEspinoza07 <arielespinoza46@gmail.com>
2023-08-09 03:18:09 -0600

    remove unused method

--- src/Auth/Domain/AuthUserName.php
+++ src/Auth/Domain/AuthUserName.php
@@ -22,11 +22,4 @@ class AuthUserName extends StringValueObject
     {
         return new static($value);
     }
-
-    public static function random(): self
-    {
-        $names = ['John', 'Peter', 'Maria'];
-
-        return new static($names[rand(0, count($names) - 1)]);
-    }
 }
